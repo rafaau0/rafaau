dcac86c3ed2d689681b88b12b881bb70e636ad3f
Rafael <rafaau0@gmail.com>
2026-09-05 13:05:43 -0300

    chore: point site download to test release

--- neiva-site/app/page.tsx
+++ neiva-site/app/page.tsx
@@ -90,7 +90,7 @@ const faqs = [
 ];
 
 const DOWNLOAD_URL =
-  'https://github.com/rafaau0/rafaau/releases/latest/download/rafaau_v1.exe';
+  'https://github.com/rafaau0/rafaau/releases/download/v1.1.1-test.1/rafaau_v1.exe';
 
 function Logo({ compact = false }: { compact?: boolean }) {
   return (
